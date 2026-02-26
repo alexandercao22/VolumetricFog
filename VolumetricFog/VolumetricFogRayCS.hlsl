5dd3cc66aa5344dbbb4f7e12b48a6dc086bb61be
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2026-02-26 15:45:08 +0100

    VolumetricFogRayCS light and shadow

--- VolumetricFog/VolumetricFogRayCS.hlsl
+++ VolumetricFog/VolumetricFogRayCS.hlsl
@@ -2,6 +2,8 @@ RWTexture2D<unorm float4> backBufferUAV : register(u0);
 
 Texture2D<float4> depthGBuffer : register(t1);
 
+#define SHADOW_EPSILON 0.0001f
+
 cbuffer camera : register(b8)
 {
     float4 camPos;
@@ -24,9 +26,11 @@ struct DirectionalLightBuffer
 
 StructuredBuffer<DirectionalLightBuffer> directionalLight : register(t7);
 Texture2DArray<float> dirShadowMaps : register(t8);
+sampler shadowMapSampler : register(s0);
 
 float PhaseHG(float cosTheta, float g)
 {
+    // https://omlc.org/classroom/ece532/class3/hg.html
     float g2 = g * g;
     return (1 - g2) / pow(1 + g2 * 2 * g * cosTheta, 1.5);
 }
@@ -52,6 +56,23 @@ float IGN(float2 pixel, int frame)
     float x = float(pixel.x) + 5.588238f * float(frame);
     float y = float(pixel.y) + 5.588238f * float(frame);
     return fmod(52.9829189f * fmod(0.06711056f * float(x) + 0.00583715f * float(y), 1.0f), 1.0f);
+}
+
+bool IsSampledPosShadowed(float3 samplePos)
+{
+    float4 lightWorldPos = mul(float4(samplePos, 1.0f), directionalLight[0].vpMatrix);
+    float2 ndcSpace = lightWorldPos.xy / lightWorldPos.w;
+    float calcDepth = lightWorldPos.z / lightWorldPos.w;
+    
+    // If the samplePos is outside the light shadow map
+    if (lightWorldPos.w <= 0.0f)
+        return false;
+    if (abs(ndcSpace.x) > 1.0f || abs(ndcSpace.y > 1.0f))
+        return false;
+    
+    float3 shadowMapUV = float3(ndcSpace.x * 0.5f + 0.5f, ndcSpace.y * -0.5f + 0.5f, 0);
+    float sampledDepth = dirShadowMaps.SampleLevel(shadowMapSampler, shadowMapUV, 0) + SHADOW_EPSILON;
+    return sampledDepth < calcDepth;
 }
 
 [numthreads(8, 8, 1)]
@@ -83,12 +104,19 @@ void main( uint3 DTid : SV_DispatchThreadID )
     float distTravelled = IGN(pixelCoords, t) * noiseOffset;
     float transmittance = 1.0f;
     
+    // Calculate lighting and shadows
+    float3 lightDir = normalize(directionalLight[0].direction); // Direction of directional light
+    float RdotL = dot(rayDir, lightDir);
+    
     // Ray-marching
     while (distTravelled < distLimit)
     {
-        if (density > 0.0f)
+        float3 sampleWorldPos = camPos.xyz + rayDir * distTravelled;
+        bool isShadowed = IsSampledPosShadowed(sampleWorldPos);
+        
+        if (density > 0.0f && !isShadowed)
         {
-            fogColor.rgb += directionalLight[0].colour * density * stepSize;
+            fogColor.rgb += directionalLight[0].colour * PhaseHG(RdotL, scattering) * density * stepSize;
             transmittance *= exp(-density * stepSize);
         }
         distTravelled += stepSize;
