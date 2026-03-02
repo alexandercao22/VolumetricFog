cc5773120e2f6f7977b83c7181211e3f83f32dd8
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2026-03-02 16:14:21 +0100

    Created Froxel lighting resources

--- VolumetricFog/Main.cpp
+++ VolumetricFog/Main.cpp
@@ -179,7 +179,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	SetupRayMarchingVolFog(device, &rayConstBuffer, &mainCamera, &rayConstData, totalSpotLights);
 
 	// Volumetric fog froxel-based
-
+	ShaderD3D11 volFogFroxelLightCS(device, ShaderType::COMPUTE_SHADER, L"VolumetricFogFroxelLight.cso");
+	ConstantBufferD3D11 volFogCamDataCB;
 
 	MSG msg = { };
 	ShowCursor(FALSE); // Hide cursor
