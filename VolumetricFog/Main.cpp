0d2dd70515e4b3311579f472c699a25270ab9ffc
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2026-03-03 14:56:56 +0100

    Run the 3d texture into the setup

--- VolumetricFog/Main.cpp
+++ VolumetricFog/Main.cpp
@@ -182,7 +182,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	ShaderD3D11 volFogFroxelLightCS(device, ShaderType::COMPUTE_SHADER, L"VolumetricFogFroxelLight.cso");
 	ConstantBufferD3D11 volFogCamDataCB;
 	ID3D11Texture3D *froxelTexture;
-	SetupFroxelVolFog(device, &volFogCamDataCB, &mainCamera, totalSpotLights);
+	SetupFroxelVolFog(device, &volFogCamDataCB, &mainCamera, totalSpotLights, froxelTexture);
 
 	MSG msg = { };
 	ShowCursor(FALSE); // Hide cursor
